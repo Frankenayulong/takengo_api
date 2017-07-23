6accea5f31ae42892e352854a962c98d6b3117a9
Kendrick Kesley <purifine@hotmail.com>
2017-07-23 15:06:11 +1000

    test commit. No purpose

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Take N Go API
                 </div>
 
                 <div class="links">
